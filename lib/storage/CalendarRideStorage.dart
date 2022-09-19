87ea6c283428c02421029a6fc4ba173f17653795
Marc Baudot <marc.baudot@gmail.com>
2022-09-19 19:48:16 +0200

    modif generales

--- lib/storage/CalendarRideStorage.dart
+++ lib/storage/CalendarRideStorage.dart
@@ -12,4 +12,17 @@ class CalendarRideStorage {
       throw Exception('Failed to load album');
     }
   }
+
+  Future<CalendarRide> fetchDriverFromDate(String data) async {
+    final response = await http.get(Uri.parse(
+        'http://fahrgemeinschaft.enconstruction.de/api/calendar_rides?page=1&date[before]=' +
+            data +
+            '&date[after]=' +
+            data));
+    if (response.statusCode == 200) {
+      return CalendarRide.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
 }
